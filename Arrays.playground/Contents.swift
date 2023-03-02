275ef0b8914d9568e86aac00512a059d4ae8e088
iosdevrashad <rashad8787@me.com>
2023-03-01 18:00:34 -0600

    Working with Arrays
    Append, Insert, and removing items from the array at the index.

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -1,3 +1,28 @@
 import UIKit
 
-var greeting = "Hello, playground"
+// LinearTime Method O(n)
+
+struct Singer {}
+
+let ints = [1, 2, 3]
+let strings = ["a", "b", "c"]
+let singer = [Singer(), Singer(), Singer()]
+
+// let ints = [Int]()
+// let strings = [String]()
+// let singer = [Singer]()
+
+// *** let arrayOfSpecificSize = Array<Int>(repeating: 1, count: 10) *** Rare that it will be used. 
+
+var array = ["a", "b", "c", "d"]
+array.remove(at: 1)
+array.insert("b", at: 1)
+array.append("e")
+
+var favoriteStates = ["Colorado", "Washington", "Montana", "California", "Nevada", "Connecticut", "Virginia", "Florida.", "New York", "Pennsylvania"]
+
+favoriteStates.remove(at: 7)
+favoriteStates.insert("Tennessee", at: 7)
+favoriteStates.append("Maryland")
+
+
