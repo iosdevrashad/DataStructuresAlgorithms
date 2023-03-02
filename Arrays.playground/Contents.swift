f65d429b837cdd02a7067a337baf1c0b0074b8e7
iosdevrashad <rashad8787@me.com>
2023-03-02 15:30:01 -0600

    Done

--- Arrays.playground/Contents.swift
+++ Arrays.playground/Contents.swift
@@ -1,28 +1,53 @@
-import UIKit
+//import UIKit
+//
+//// LinearTime Method O(n)
+//
+//struct Singer {}
+//
+//let ints = [1, 2, 3]
+//let strings = ["a", "b", "c"]
+//let singer = [Singer(), Singer(), Singer()]
+//
+//// let ints = [Int]()
+//// let strings = [String]()
+//// let singer = [Singer]()
+//
+//// *** let arrayOfSpecificSize = Array<Int>(repeating: 1, count: 10) *** Rare that it will be used.
+//
+//var array = ["a", "b", "c", "d"]
+//array.remove(at: 1)
+//array.insert("b", at: 1)
+//array.append("e")
+//
+//var favoriteStates = ["Colorado", "Washington", "Montana", "California", "Nevada", "Connecticut", "Virginia", "Florida.", "New York", "Pennsylvania"]
+//
+//favoriteStates.remove(at: 7)
+//favoriteStates.insert("Tennessee", at: 7)
+//favoriteStates.append("Maryland")
 
-// LinearTime Method O(n)
+/*
+ Rotate array to right N times.
+ https://app.codility.com/programmers/lessons/2-arrays/cyclic_rotation/
+ 
+ For example, given
 
-struct Singer {}
+     A = [3, 8, 9, 7, 6]
+     K = 3
+ the function should return [9, 7, 6, 3, 8]. Three rotations were made:
 
-let ints = [1, 2, 3]
-let strings = ["a", "b", "c"]
-let singer = [Singer(), Singer(), Singer()]
+     [3, 8, 9, 7, 6] -> [6, 3, 8, 9, 7]
+     [6, 3, 8, 9, 7] -> [7, 6, 3, 8, 9]
+     [7, 6, 3, 8, 9] -> [9, 7, 6, 3, 8]
 
-// let ints = [Int]()
-// let strings = [String]()
-// let singer = [Singer]()
+ */
 
-// *** let arrayOfSpecificSize = Array<Int>(repeating: 1, count: 10) *** Rare that it will be used. 
+func solution(A: [Int], K: Int) -> [Int] {
+   
+    return [Int]()
+}
 
-var array = ["a", "b", "c", "d"]
-array.remove(at: 1)
-array.insert("b", at: 1)
-array.append("e")
+solution(A: [1, 2, 3, 4, 5], K: 1) // 5 1 2 3 4
+solution(A: [1, 2, 3, 4, 5], K: 2) // 4 5 1 2 3
+solution(A: [1, 2, 3, 4, 5], K: 3) // 3 4 5 1 2
 
-var favoriteStates = ["Colorado", "Washington", "Montana", "California", "Nevada", "Connecticut", "Virginia", "Florida.", "New York", "Pennsylvania"]
-
-favoriteStates.remove(at: 7)
-favoriteStates.insert("Tennessee", at: 7)
-favoriteStates.append("Maryland")
-
-
+solution(A: [3, 8, 9, 7, 6], K: 3) // [9, 7, 6, 3, 8]
